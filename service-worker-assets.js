e753a2183121167669ef9b033cb802f4619e2f7c
Ryota-TAMURA832 <tamura.ryota832@gmail.com>
2026-04-26 02:04:42 +0900

    Use share sheet for PDF output on iPhone

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -193,7 +193,7 @@
       "url": "icon-512.png"
     },
     {
-      "hash": "sha256-FwuCpEq+2\/x975uK+EpjVCugfFQflq3px9blrIJVuHg=",
+      "hash": "sha256-dGh43IgwWO+KuPGusgWi++mfhMKRu2l4lliQnM9kZAE=",
       "url": "index.html"
     },
     {
@@ -209,5 +209,5 @@
       "url": "sample-data\/weather.json"
     }
   ],
-  "version": "2X16x+44"
+  "version": "U6hRT0Lz"
 };
--- service-worker.js
+++ service-worker.js
@@ -53,4 +53,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: 2X16x+44 */
+/* Manifest version: U6hRT0Lz */
